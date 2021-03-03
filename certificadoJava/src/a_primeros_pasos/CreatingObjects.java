2f1479df55343d8f52ad1958f5272151b9c87138
Pedro Saez <pipomadrid@gmail.com>
2021-03-03 11:52:35 +0100

    Crendo objetos

---
+++ certificadoJava/src/a_primeros_pasos/CreatingObjects.java
@@ -0,0 +1,44 @@
+package a_primeros_pasos;
+
+import java.util.Random;
+
+// remember that an object is an instance of a class
+
+public class CreatingObjects {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Random r = new Random(); // we are creating an instance of Random and storing it in r variable
+
+		// () tell us is a constructor , that is a special method that creates a new
+		// object
+
+		// define a new constructor :
+		
+		//for most classes you don't have to code a constructor, the compiler will supply a default constructor for you
+
+	}
+
+	public class Chick {
+
+		public Chick() { // the name of the constructor matches the name of the class, and there is no
+							// return type
+			System.out.println("in constructor");
+		}
+
+		// the purpose if the constructor is to inicialize fields, you can inicialize in
+		// the constructor or directly
+		// on the line on which they're declared
+
+	}
+
+	public class Chicken {
+		int numEggs = 0;// initialize on line
+		String name;
+
+		public Chicken() {
+			name = "Duke";// initialize in constructor
+		}
+	}
+}
