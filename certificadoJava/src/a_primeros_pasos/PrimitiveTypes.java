7eb5f73924660edce980009b2c83888d20217880
Pedro Saez <pipomadrid@gmail.com>
2021-04-07 10:17:41 +0200

    primitive types completed

--- certificadoJava/src/a_primeros_pasos/PrimitiveTypes.java
+++ certificadoJava/src/a_primeros_pasos/PrimitiveTypes.java
@@ -29,8 +29,20 @@ public class PrimitiveTypes {
  	 * octal(0-7) use 0 as prefix 017
  	 *  hexadecimal(0-9) use 0 folloewd by x as a prefix 0xFF
  	 *  binary(0-1) number 0 folloewd by b as prefix 0b10
+ 	 *  
+ 	 *  you can underscore numbers to make them more readable
+ 	 *  int million1 = 1000000;
+     *  int million2 = 1_000_000;
+     *  You can add
+		underscores anywhere except at the beginning of a literal, the end of a literal, right before a
+		decimal point, or right after a decimal point. Let’s look at a few examples:
+		double notAtStart = _1000.00; // DOES NOT COMPILE
+		double notAtEnd = 1000.00_; // DOES NOT COMPILE
+		double notByDecimal = 1000_.00; // DOES NOT COMPILE
+		double annoyingButLegal = 1_00_0.0_0; // this one compiles
+			
+     *  
 	 */
-	
 	
 	public static void main(String[] args) {
 		
