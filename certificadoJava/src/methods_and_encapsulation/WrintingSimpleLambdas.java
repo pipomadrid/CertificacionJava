3214df0538a9a943a0765da9ffec880506fae43e
Pedro <pipomadrid@gmail.com>
2021-09-12 23:39:52 +0200

    aun no acabado lambdas

--- certificadoJava/src/methods_and_encapsulation/WrintingSimpleLambdas.java
+++ certificadoJava/src/methods_and_encapsulation/WrintingSimpleLambdas.java
@@ -1,10 +1,56 @@
 package methods_and_encapsulation;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class WrintingSimpleLambdas {
+	
+	//Added in Java 8
+	/*
+	 * You  can think of a lambda expression as an anonymous method. doesn't have a name.
+	 * Lambda expression is like a method that you can pass as if it were a variable.
+	 * Only the simplest lambda expressions are on the OCA exam
+	 */
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		List<Animal> animals = new ArrayList<Animal>(); // list of animals
+		 animals.add(new Animal("fish", false, true));
+		 animals.add(new Animal("kangaroo", true, false));
+		 animals.add(new Animal("rabbit", true, false));
+		 animals.add(new Animal("turtle", false, true));
+		 
+		 print(animals,a -> a.canHop());
+		 
+	}
+	
+	private static void print(List<Animal> animals, CheckTrait checker) {
+		 for (Animal animal : animals) {
+		 if (checker.test(animal)) // the general check
+		 System.out.print(animal + " ");
+		 }
+	}
+}
 
+class Animal {
+	private String species;
+	private boolean canHop;
+	private boolean canSwim;
+	public Animal(String speciesName, boolean hopper, boolean swimmer) {
+		species = speciesName;
+		canHop = hopper;
+		canSwim = swimmer;
+	}
+	public boolean canHop() { return canHop; }
+	public boolean canSwim() { return canSwim; }
+	public String toString() { return species; }
 	}
 
+
+interface CheckTrait{
+	boolean test(Animal a);	
 }
+
+
+
+
