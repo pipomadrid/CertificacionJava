c15e65391e74bd129d929230b758fa7dd794681e
Pedro Saez <pipomadrid@gmail.com>
2021-09-12 12:16:22 +0200

    terminado creating inmutable class

---
+++ certificadoJava/src/methods_and_encapsulation/CreatingInmutableClasses.java
@@ -0,0 +1,72 @@
+package methods_and_encapsulation;
+
+/*Encapsulating data is helpful because it prevents callers from making uncontrolled changes
+to your class.Another common technique is making classes immutable so they cannot be
+changed at all.
+you can pass them around your application with a guarantee that the caller didn’t change anything.
+This helps make programs easier to maintain
+*/
+
+public class CreatingInmutableClasses {
+
+	public static void main(String[] args) {
+
+
+		StringBuilder sb = new StringBuilder(" initial");
+		
+		NotImmutable example = new NotImmutable(sb);
+		sb.append(" added");
+		StringBuilder gotBuilder = example.getBuilder();
+		sb.append(" more");
+		System.out.println(gotBuilder);
+		
+		System.out.println(sb);
+		
+		Immutable example2 = new Immutable(sb);
+		sb.append(" que tal");//change the StringBuilder but not the String 
+	    String gotString = example2.getValue();
+	    sb.append("adding again");
+	    System.out.println(gotString);
+	}
+
+}
+
+//One step in making a class immutable is to omit the setters.
+
+//immutable is only measured after the object is constructed so with the constructor we can pass a value to the class
+
+class ImmutableSwan {
+	
+	private int numberEggs;
+	public ImmutableSwan(int numberEggs) {
+		this.numberEggs = numberEggs;
+	}
+	public int getNumberEggs() {
+		return numberEggs;
+	} }
+ 
+// we have to be carefull with what we return, if we return a stringbuilder, we can add values in it
+// making changes in the "inmutable" class
+
+class NotImmutable {
+	private StringBuilder builder; 
+	public NotImmutable(StringBuilder b) {
+		builder = b;
+	}
+	public StringBuilder getBuilder() {  //We are returning a mutable object, so we can change it after the call
+		return builder;
+
+	}
+}
+
+class Immutable {
+	
+	private StringBuilder builder2; 
+	public Immutable(StringBuilder b) {
+		builder2 = b;
+	}
+	public String getValue() { //now we are returning a String, and we can't change it
+		return builder2.toString();
+
+	}
+}
--- certificadoJava/src/methods_and_encapsulation/EncapsulationData.java
+++ certificadoJava/src/methods_and_encapsulation/EncapsulationData.java
@@ -1,5 +1,10 @@
 package methods_and_encapsulation;
 
+
+/*
+ * Encapsulation means we set up the class so only methods
+	in the class with the variables can refer to the instance variables
+ */
 public class EncapsulationData {
 
 	public static void main(String[] args) {
@@ -8,3 +13,57 @@ public class EncapsulationData {
 	}
 
 }
+class Swan {
+	
+	private int numberEggs; //private
+	public int getNumberEggs() {//getter
+		return numberEggs;
+	}
+	public void setNumberEggs(int numberEggs) {//setter
+		if(numberEggs >= 0 ) this.numberEggs = numberEggs;
+		
+	}
+	
+}
+
+/*
+ * 
+ * Java defines a naming convention that is used in JavaBeans. 
+ * JavaBeans are reusable software components. 
+ * JavaBeans call an instance variable a property. 
+ * The only thing you need to know about JavaBeans 
+ * for the exam is the naming conventions listed in Table
+ * 
+ * JAVABEANS CONVENTIONS:
+ * 
+ * Properties are private. private int numEggs;
+Getter methods begin with is if the property is a boolean . public boolean isHappy() {
+																	return happy;
+																}
+Getter methods begin with get if the
+property is not a boolean . 								public int getNumEggs() {
+																	return numEggs;
+															}
+Setter methods begin with set . 							public void setHappy(boolean happy) {
+																	this.happy = happy;
+															}	
+The method name must have a prefix
+of set / get / is , followed by the first
+letter of the property in uppercase, fol-
+lowed by the rest of the property name. 					public void setNumEggs(int num) {
+																	numEggs = num;
+															}
+ * 
+ */
+
+/*
+ * 
+ * 	private boolean playing;  
+	private String name;
+	public boolean getPlaying() { return playing; }  // should be isPlaying because is  boolean
+	public boolean isPlaying() { return playing; }  //Correct
+	public String name() { return name; }  // should be getName 
+	public void updateName(String n) { name = n; }// should be getName 
+	public void setname(String n) { name = n; }  // should be getName 
+	 
+*/
