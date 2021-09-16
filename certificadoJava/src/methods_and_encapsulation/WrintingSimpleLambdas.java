6c716c2124f2d315b58c4461134cb885589c0dd2
Pedro <pipomadrid@gmail.com>
2021-09-16 18:39:07 +0200

    predicates

--- certificadoJava/src/methods_and_encapsulation/WrintingSimpleLambdas.java
+++ certificadoJava/src/methods_and_encapsulation/WrintingSimpleLambdas.java
@@ -26,7 +26,42 @@ public class WrintingSimpleLambdas {
 		  * if we didn't use lambda we have to write classes that implements the method of the interface to check the animals that
 		  * can swim too, the lambda simplifies the code.  
 		  */
-		 print (animals,a ->a.canSwim());
+		 print (animals,a ->a.canSwim()); 
+		 print (animals,a ->!a.canSwim());
+		 
+		 /*
+		  *  the print method expects a CheckTrait method and we are passing a lambda, 
+		  *  java tries to map our lambda to the interface: boolean test(Animal a)
+		  *  that means that lambda parameter must be an Animal and has to return a boolean.
+		  *  
+		  *  The syntax of lambdas is tricky because many parts are optional
+		  *  
+		  *  
+		  * 	 a -> a.canHop()
+				(Animal a) -> { return a.canHop(); }
+				
+				The parentheses can only be omitted if there is a single parameter.
+				We can omit braces when we only have a single statement.
+				Here java doesn’t require you to type return or use
+				a semicolon when no braces are used.
+								
+				 print(() -> true); // 0 parameters
+				 print(a -> a.startsWith("test")); // 1 parameter
+				 print((String a) -> a.startsWith("test")); // 1 parameter
+				 print((a, b) -> a.startsWith("test")); // 2 parameters
+				 print((String a, String b) -> a.startsWith("test")); // 2 parameters
+				 print(a -> { return a.startsWith("test"); });
+				 
+				 There is one more issue you might see with lambdas. We’ve been defining an argument
+				list in our lambda expressions. Since Java doesn’t allow us to redeclare a local variable, the
+				following is an issue:
+				(a, b) -> { int a = 0; return 5;} // DOES NOT COMPILE
+				We tried to redeclare a, which is not allowed. By contrast, the following line is okay
+				because it uses a different variable name:
+				(a, b) -> { int c = 0; return 5;}
+		  */
+		 
+		 
 	}
 	
 	private static void print(List<Animal> animals, CheckTrait checker) {
